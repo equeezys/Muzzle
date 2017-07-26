7652ef8f81f1e8e16b81ca457929c3db3d0ef716
Vlad <tvl95@bk.ru>
2017-07-26 15:18:58 +0300

    level1

--- MusicMaker/js/main.js
+++ MusicMaker/js/main.js
@@ -1,7 +1,8 @@
 var q=0,
 _sor=document.getElementById("musicList"),
 _player = document.getElementById("player"),
-countUpdate=0;
+countUpdate=0,
+iMus=0;
 
 window.onload = function() {
     // TODO:: Do your initialization job
@@ -29,7 +30,20 @@ window.onload = function() {
     	}
     	//alert(1);
     });
-    document.getElementById("player").addEventListener("ended", playNext);
+    document.getElementById("player").addEventListener("ended", function(e) {
+    	//alert("Event Listener "+iMus);
+        if (iMus>0) {
+        	playNext();
+            if(iMus < 4) playlistItemClick(e.target);
+            	else {
+            		alert("Congrats you win!Your score : "+countUpdate+" .");
+            		iMus=0;
+            }
+          }
+        else     	playNext();
+    	//alert(1);
+    });
+    
        $( "#sortablePlaylist" ).sortable({
     	   change: function( event, ui ) {
     		   countUpdate++;
@@ -46,10 +60,19 @@ function playlistItemClick(clickedElement) {
         selected.classList.remove("selected");
     }
     clickedElement.classList.add("selected");
-    if(clickedElement.id=="playSortList"){
+    //alert("ItemClick"+iMus);
+    if(iMus>0 || clickedElement.id=="playSortList"){
     	var sortedIDs = $( "#sortablePlaylist" ).sortable( "toArray" );
-    	alert(sortedIDs[0]);
-    	q=0;
+    	if(sortedIDs[iMus]=="item"+iMus){
+        	_player2.src = document.getElementById(sortedIDs[iMus]).getAttribute("src");
+        	iMus++;
+        	_player2.play();
+    	}
+    	else{
+    		alert("WROOONG!!! "+iMus);
+    		iMus=0;
+    		playNext();
+    	}
     	
     }
     else{
